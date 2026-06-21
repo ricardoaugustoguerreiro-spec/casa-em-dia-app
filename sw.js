bbf4a2754ff6ef6022fb2e6e687da842562c5507
Ricardo Augusto <ricardoaugustoguerreiro@gmail.com>
2026-06-21 16:10:06 -0300

    Adiciona icones PNG (iOS/Android), ajustes de tela e cache do PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = "casa-em-dia-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./js/config.js", "./js/supabaseClient.js", "./js/app.js", "./icons/icon.svg"];
+const CACHE = "casa-em-dia-v2";
+const ASSETS = [
+  "./", "./index.html", "./manifest.json",
+  "./js/config.js", "./js/supabaseClient.js", "./js/app.js",
+  "./icons/icon.svg", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/icon-180.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
